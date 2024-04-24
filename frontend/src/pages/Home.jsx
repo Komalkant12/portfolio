fdd945a140a674c16acb60964d8e05ff9224bbe5
Komalkant Deshmukh <komalkantdeshmukh112@gmail.com>
2024-04-24 08:08:42 +0530

    card created

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PortfolioCard from "../components/PortfolioCard"
+const Home = () => {
+  return (
+    <div className=''>
+      <PortfolioCard/>
+    </div>
+  )
+}
+
+export default Home
